f4e664456c84bde9a3f637cd03e2aa411807f1a9
Steven Pak <spak9@gmu.edu>
2020-11-26 21:42:01 -0500

    created a quick interface activity for leds & temp

---
+++ app/src/main/java/com/example/androidesp8266/InterfaceActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidesp8266;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InterfaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface);
+    }
+}
--- app/src/main/java/com/example/androidesp8266/MainActivity.java
+++ app/src/main/java/com/example/androidesp8266/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.androidesp8266;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button houseInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        houseInterface = (Button) findViewById(R.id.houseInterface);
+
+         // When the button is clicked, start the InterfaceActivity
+        houseInterface.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeHouseInterface();
+            }
+        });
+    }
+
+    /*
+    The only method on this Activity. It should just create an Intent
+    and start a new Activity that will be the interface of both
+    1. LEDs on/off
+    2. DHT11 Temperature/humitity GET
+     */
+    public void seeHouseInterface() {
+        Intent intent = new Intent(this, InterfaceActivity.class);
+        startActivity(intent);
     }
 }
